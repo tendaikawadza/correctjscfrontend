3ffd5739356b433594604c06cb564b6c75166315
tendai <tendaikawadza@gmail.com>
2023-02-16 23:37:12 -0800

    update

---
+++ src/app/te/te.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TeComponent } from './te.component';
+
+describe('TeComponent', () => {
+  let component: TeComponent;
+  let fixture: ComponentFixture<TeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/te/te.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-te',
+  templateUrl: './te.component.html',
+  styleUrls: ['./te.component.css']
+})
+export class TeComponent {
+
+}
